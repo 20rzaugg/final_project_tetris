104bdb457d0c8c59d2260ddcb322bb4440b16812
Ashton <ashtongee1@gmail.com>
2022-12-01 16:58:09 -0700

    the boys are all here

---
+++ VGA/score.vhd
@@ -0,0 +1,119 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+module score is 
+    port (
+        clk, rst_l : in std_logic;
+        score : in unsigned(20 downto 0);
+        score_digits : out array(0 to 5) of unsigned(3 downto 0);
+    )
+end module;
+
+architecture behavioral of score is
+
+    component divider is port (
+        denom : IN STD_LOGIC_VECTOR (19 DOWNTO 0);
+        numer : IN STD_LOGIC_VECTOR (19 DOWNTO 0);
+        quotient : OUT STD_LOGIC_VECTOR (19 DOWNTO 0);
+        remain : OUT STD_LOGIC_VECTOR (19 DOWNTO 0)
+    );
+    END component;
+
+    type state is (delay, set);
+    signal state, nextstate : state := delay;
+    signal score_reg : std_logic_vector(19 downto 0);
+    signal denominator_reg : std_logic_vector(19 downto 0) := X"00001";
+    signal quotient_reg : std_logic_vector(19 downto 0);
+    signal remainder_reg : std_logic_vector(19 downto 0);
+    signal digit : unsigned(3 downto 0) := X"0";
+
+begin
+
+    --instantiate divider module
+    U1_divider : divider port map (
+        denom => denominator_reg;
+        numer => score_reg;
+        quotient => quotient_reg;
+        remain => remainder_reg;
+    )
+    
+    --state machine, bring the future to the present
+    process(clk, rst_l)
+    begin
+        if rst_l = '0' then
+            state <= delay;
+            score_reg <= (others => '0');
+        elsif rising_edge(clk) then
+            state <= nextstate;
+        end if;
+    end process;
+
+    process(state)
+    begin
+        case state is
+            when delay =>
+                if(digit < X"5")
+                    nextstate <= set;
+                else
+                    nextstate <= delay;
+                end if;
+            when set =>
+                case digit is
+                    when X"0" =>
+                        --save the hundred-thousands digit
+                        score_digits(0) <= unsigned(quotient_reg(3 downto 0));
+                        score_reg <= remainder_reg;
+                        --divide the remainder by 10k
+                        denominator_reg <= X"02710"; --10000
+                        digit <= digit + X"1";
+                        nextstate <= delay;
+                    when X"1" =>
+                        --save the ten-thousands digit
+                        score_digits(1) <= unsigned(quotient_reg(3 downto 0));
+                        score_reg <= remainder_reg;
+                        --divide the remainder by 1k
+                        denominator_reg <= X"003E8"; --1000
+                        digit <= digit + X"1";
+                        nextstate <= delay;
+                    when X"2" =>
+                        --save the thousands digit
+                        score_digits(2) <= unsigned(quotient_reg(3 downto 0));
+                        score_reg <= remainder_reg;
+                        --divide the remainder by 100
+                        denominator_reg <= X"00064"; --100
+                        digit <= digit + X"1";
+                        nextstate <= delay;
+                    when X"3" =>
+                        --save the hundreds digit
+                        score_digits(3) <= unsigned(quotient_reg(3 downto 0));
+                        score_reg <= remainder_reg;
+                        --divide the remainder by 10
+                        denominator_reg <= X"0000A"; --10
+                        digit <= digit + X"1";
+                        nextstate <= delay;
+                    when X"4" =>
+                        --save the tens digit and ones digit
+                        score_digits(4) <= unsigned(quotient_reg(3 downto 0));
+                        score_digits(5) <= unsigned(remainder_reg(3 downto 0));
+                        score_reg <= others('0');
+                        denominator_reg <= X"00001"; --1
+                        digit <= digit + X"1";
+                        nextstate <= delay;
+                    when others =>
+                        nextstate <= delay;
+                end case; 
+        end case;
+    end process;
+
+    --when the score changes, start the state machine
+    process(score)
+    begin
+        digit <= X"0";
+        score_reg <= std_logic_vector(score);
+        --divide the score by 100k
+        denominator_reg <= X"186A0"; --100,000
+
+    end process;
+
+end behavioral;
--- controller.vhd
+++ controller.vhd
@@ -21,109 +21,189 @@ end entity controller;
 architecture behavioral of controller is 
 	
 	--STATE SIGNALS
-	type state_type is ();
-	signal cur_state : state_type;
-	signal next_state : state_type;
-	signal row, col : unsigned(3 downto 0);
+	type state_type is (play, game_over, initial);
+	signal cur_state, next_state : state_type;
+	--Other Signals
+	signal row, col, next_row, next_col : unsigned(3 downto 0);
 	signal timer : integer;
-	signal checkArray : boolean := False;
+	signal checkArray, next_checkArray : boolean := False;
+	signal Barray, next_Barray : array(0 to 8, 0 to 11) of unsigned(2 downto 0);
+	signal fBlock, next_fBlock : unsigned(2 downto 0);
+	signal scorn : next_scorn : unsigned(19 downto 0);
+	signal createBlock, next_createBlock : boolean := false;
 
 begin
 
 	--signal initialization
 	falling_block_col <= col;
 	falling_block_row <= row;
+	blockArray <= Barray;
+	falling_block <= fBlock;
+	score <= scorn;
 		
 	
 	
 	process (MAX10_CLK1_50)
 	begin
-	
-		--col logic
-		case BoxPosition is
-			when => >X"1C7"
-				if blockArray(X"0", row) = X"0" then
-					col <= X"0";
-				else
-					col <= col;
-				end if;
-			when => <=X"1C7" and >X"38E"
-				if blockArray(X"1", row) = X"0" then
-					col <= X"1";
-				else
-					col <= col;
-				end if;
-			when => <=X"38E" and >X"555"
-				if blockArray(X"2", row) = X"0" then
-					col <= X"2";
-				else
-					col <= col;
-				end if;
-			when => <=X"555" and >X"71C"
-				if blockArray(X"3", row) = X"0" then
-					col <= X"3";
-				else
-					col <= col;
-				end if;
-			when => <=X"71C" and >X"8E3"
-				if blockArray(X"4", row) = X"0" then
-					col <= X"4";
-				else
-					col <= col;
-				end if;
-			when => <=X"8E3" and >X"AAA"
-				if blockArray(X"5", row) = X"0" then
-					col <= X"5";
-				else
-					col <= col;
-				end if;
-			when => <=X"AAA" and >X"C71"
-				if blockArray(X"6", row) = X"0" then
-					col <= X"6";
-				else
-					col <= col;
-				end if;
-			when => <=X"C71" and >X"E38"
-				if blockArray(X"7", row) = X"0" then
-					col <= X"7";
-				else
-					col <= col;
-				end if;
-			when => <=X"E38" and >=X"FFF"
-				if blockArray(X"8", row) = X"0" then
-					col <= X"8";
-				else
-					col <= col;
-				end if;
-		end case;
-		
-		--row logic
-		if timer = 25000000 then
-			row <= row - X"1";
-			if row = X"0" or blockArray(col, row - 1) != X"0" then --0 means black, if not black it is filled.
-				blockArray(col, row) <= falling_block;
-				checkArray <= true;
-				falling_block <= rand;
-			end if;
+		if key(0) = '0' then
+			cur_state <= initial;
+		else
+			fBlock <= next_fBlock;
+			Barray <= next_Barray;
+			checkArray <= next_checkArray;
+			row <= next_row;
+			col <= next_col;
+			cur_state <= next_state;
+			scorn <= next_scorn;
+			createBlock <= next_createBlock;
 		end if;
-		
-		--array Management
-		if checkArray = true then
-			for(int i = 0; i < 8; i++) --check rows
-				for(int j = 0; j < 11; j++)--check cols
-					if blockArray(i,j) != X"0" and blockArray(i,j) = blockArray(i + 1, j) and blockArray(i,j) = blockArray(i + 2, j) then
-						score <= score + X"3";
-					
-					
-	
-
-			
-	
 	end process;
 	
 	
-	process()
+	process(col, row, Barray, checkArray, fBlock)
 		begin
+		next_fBlock <= fBlock;
+		next_Barray <= Barray;
+		next_checkArray <= checkArray;
+		next_row <= row;
+		next_col <= col;
+		next_state <= cur_state;
+		next_scorn <= scorn;
+		next_createBlock <= createBlock;
+		
+		case cur_state is
+			when initial =>
+				for i in 0 to 8 loop
+					for j in 0 to 11 loop
+						next_blockArray(i,j) <= X"0";
+					end loop;
+				end loop;
+				
+				next_score <= 0;
+				
+				if key(1) = '0' then
+					next_state <= play;
+					next_createBlock <= true;
+				end if;
+			
+			when play =>
+			
+				--initialize falling block
+				if createBlock = true then
+					next_fBlock <= rand;
+					next_row <= X"D";
+					next_col <= X"4";
+					next_createBlock <= false;
+				end if;
+		
+				--col logic
+				case BoxPosition is
+					when => >X"1C7"
+						if Barray(X"0", row) = X"0" then
+							next_col <= X"0";
+						else
+							next_col <= col;
+						end if;
+					when => <=X"1C7" and >X"38E"
+						if Barray(X"1", row) = X"0" then
+							next_col <= X"1";
+						else
+							next_col <= col;
+						end if;
+					when => <=X"38E" and >X"555"
+						if Barray(X"2", row) = X"0" then
+							next_col <= X"2";
+						else
+							next_col <= col;
+						end if;
+					when => <=X"555" and >X"71C"
+						if Barray(X"3", row) = X"0" then
+							next_col <= X"3";
+						else
+							next_col <= col;
+						end if;
+					when => <=X"71C" and >X"8E3"
+						if Barray(X"4", row) = X"0" then
+							next_col <= X"4";
+						else
+							next_col <= col;
+						end if;
+					when => <=X"8E3" and >X"AAA"
+						if Barray(X"5", row) = X"0" then
+							next_col <= X"5";
+						else
+							next_col <= col;
+						end if;
+					when => <=X"AAA" and >X"C71"
+						if Barray(X"6", row) = X"0" then
+							next_col <= X"6";
+						else
+							next_col <= col;
+						end if;
+					when => <=X"C71" and >X"E38"
+						if Barray(X"7", row) = X"0" then
+							next_col <= X"7";
+						else
+							next_col <= col;
+						end if;
+					when => <=X"E38" and >=X"FFF"
+						if Barray(X"8", row) = X"0" then
+							next_col <= X"8";
+						else
+							next_col <= col;
+						end if;
+				end case;
+				
+				--row logic
+				if timer = 25000000 then
+					next_row <= row - X"1";
+					timer <= 0;
+					if row = X"0" or Barray(col, row - 1) != X"0" then --0 means black, if not black it is filled.
+						next_BArray(col, row) <= fBlock;
+						next_checkArray <= true;
+						next_createBlock <= true;
+					else if Barray(col, row) = X"C" then --check the array if at 12, switch state to game over.
+						next_state <= game_over;
+					end if;
+				else
+					timer <= timer + 1;
+				end if;
+				
+				--array Management
+				if checkArray = true then
+					for i in 0 to 8 loop--for(int i = 0; i < 8; i++) --check rows
+						for j in 0 to 11 loop--for(int j = 0; j < 11; j++)--check cols
+							if blockArray(i,j) != X"0" and blockArray(i,j) = blockArray(i + 1, j) and blockArray(i,j) = blockArray(i + 2, j) and i < 7 then
+								if blockArray(i + 3, j) = blockArray(i,j) and i < 6 then
+									if blockArray(i + 4, j = blockArray(i,j) and i < 5 then
+										next_scorn <= scorn + X"5";
+									else 
+										next_scorn <= scorn + X"4";
+									end if;
+								else
+									next_scorn <= scorn + X"3";
+								end if;
+							else if blockArray(i,j) != X"0" and blockArray(i,j) = blockArray(i, j + 1) and blockArray(i,j) = blockArray(i, j + 2) and j < 9 then
+								if blockArray(i, j + 3) = blockArray(i,j) and j < 8 then
+									next_scorn <= scorn + X"4";
+								else
+									next_scorn <= scorn + X"3";
+								end if;
+							else if i > 1 and blockArray(i,j) != X"0" and blockArray(i - 1 ,j) = X"0" then
+								next_blockArray(i - 1, j) = blockArray(i,j);
+								next_blockArray(i,j) = X"0";
+							end if
+						end loop;
+					end loop;
+					next_checkArray <= False;
+				end if;
+				
+			when game_over =>
+				next_fBlock <= X"0";
+				next_row <= X"D";
+				next_col <= X"4";
+				next_createBlock <= false;	
+		end case;
 		
 	end process;
 	
